d06c871d1048392c83188c7f5d72679d121d1b54
Lukasz Cieply <hurricane.pl.96@gmail.com>
2016-07-11 01:12:37 +0200

    working class macierz with operator<<

--- main.c
+++ main.c
@@ -2,31 +2,50 @@
 
 using namespace std;
 
-class macierz{
+
+
+class macierz
+{
 private:
-  int ** tab;
+  int** tab;
+  int x,y;
+
 public:
-  macierz(int x,int y){
-
+  macierz(int xx,int yy):x(xx),y(yy)
+  {
     tab= new int*[y];
-    for(int foo=0;foo<y;foo++)tab[y]=new int[x];
-
+    for(int foo=0;foo<y;foo++)  tab[foo]=new int[x];
     cout<<"Podaj dane:\n";
-    for(int foo=0;foo<y;foo++){
+    for(int foo=0;foo<y;foo++)
+    {
       cout<<"linia: "<<foo+1<<endl;
-      for(int bar=0;bar<x;bar++)cin>>tab[y][x];
+      for(int bar=0;bar<x;bar++)
+      {
+        cin>>tab[foo][bar];
+      }
       cout<<endl;
     }
-
   }
 
+  int get(int x, int y) const {  return tab[y][x]; }
+  int getX() const { return x; }
+  int getY() const { return y; }
+
+friend ostream& operator<<(std::ostream& stream, const macierz& maci);
 };
 
-int main(){
+ostream& operator<<(std::ostream& stream, const macierz& maci) {
+  for(int foo=0;foo<maci.getY();foo++)
+  {
+    cout<<endl;
+    for(int bar=0;bar<maci.getX();bar++)cout<<maci.get(bar,foo)<<" ";
+  }
+}
 
-  cout<<"Hello world"<<endl;
-
+int main()
+{
   macierz maci(3,2);
+  cout<<maci;
 
   char c;
   cin>>c;
