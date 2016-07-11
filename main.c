9c8a36aefdda39af8b2eb96a733482de918c038d
Lukasz Cieply <hurricane.pl.96@gmail.com>
2016-07-11 20:35:48 +0200

    added constructor to initizlize macierze with array

--- main.c
+++ main.c
@@ -13,7 +13,8 @@ public:
   macierz(int xx,int yy):x(xx),y(yy)
   {
     tab= new T*[y];
-    for(int foo=0;foo<y;foo++)  tab[foo]=new T[x];
+    for(int foo=0;foo<y;foo++)  tab[foo]=new T[x];//creating 2D array
+
     for(int foo=0;foo<y;foo++)
     {
       cout<<"linia: "<<foo+1<<endl;
@@ -25,9 +26,24 @@ public:
     }
   }
 
+  macierz(int xx,int yy,T* inputArray):x(xx),y(yy){
+    tab= new T*[y];
+    for(int foo=0;foo<y;foo++)  tab[foo]=new T[x];//creating 2D array
+
+    int it=0;
+    for(int foo=0;foo<y;foo++)
+    {
+      for(int bar=0;bar<x;bar++)
+      {
+        tab[foo][bar]=inputArray[it++];
+      }
+  }
+}
+
   T get(int x, int y) const {  return tab[y][x]; }
   int getX() const { return x; }
   int getY() const { return y; }
+
 
 template<typename Te> friend ostream& operator<<(ostream& , const macierz<Te>& );
 };
@@ -55,17 +71,18 @@ class macierzkw
 {
 public:
   macierzkw(int x):macierz<T>(x,x){}
+  macierzkw(int x,T* tabl):macierz<T>(x,x,tabl){}
 
   T wyznacznik() const
   {
     T w=0;
-    if(this->getX()==2  &&  this->getY()==2)
+    if(this->getX()==2)
     {
       w=this->get(0,0)*this->get(1,1);
       w-=this->get(0,1)*this->get(1,0);
       return w;
     }
-    if(this->getX()==3 &&  this->getY()==3)//  Metoda Sarrusa
+    else if(this->getX()==3)//  Metoda Sarrusa
     {
       w=this->get(0,0)*this->get(1,1)*this->get(2,2);
       w+=this->get(1,0)*this->get(2,1)*this->get(0,2);
@@ -77,14 +94,18 @@ public:
 
       return w;
     }
+    //else if()
   }
 };
 
 int main()
 {
-  macierzkw<long> xyz(3);
+  //macierzkw<long> xyz(3);
+  int tab[]={1,2,1,2};
+  macierzkw<int> abc(2,tab);
 
-  cout<<xyz.wyznacznik();
+
+  cout<<abc.wyznacznik();
 
   char c;
   cin>>c;
